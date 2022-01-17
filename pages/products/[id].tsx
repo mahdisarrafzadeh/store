ca9e5491999a4f8a2e24f092dc89e39278307736
mahdi <mahdisarrafz9@gmail.com>
2022-01-17 20:18:54 +0330

    use redux product

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { productActionCreator } from '../../redux/action/products.action';
 
 interface Props {
 
 }
 
 const Products = (props: Props) => {
+
+    const dispatch = useDispatch();
+    const router = useRouter()
+    const { id } = router.query
+    useEffect(() => {
+        if (id) {
+            dispatch(productActionCreator(Number(id)))
+        }
+    }, [id])
     return (
         <div>
+
         </div>
     )
 }
